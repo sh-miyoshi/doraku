f705af0d191dafc96e386b9574d1bcfdad84d276
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2019-04-05 00:28:28 +0000

    add struct for frontend

--- backend/pkg/hobbyapi/v1/types.go
+++ backend/pkg/hobbyapi/v1/types.go
@@ -1,6 +1,6 @@
 package hobbyapi
 
-// Hobby struct defines info of hobby
+// Hobby struct defines info of hobby details
 type Hobby struct {
 	ID          int    `json:"id"`
 	Name        string `json:"name"`
@@ -8,4 +8,10 @@ type Hobby struct {
 	Description string `json:"description"`
 	Image       string `json:"image"`
 	Group       int    `json:"group"`
+}
+
+// HobbyKey struct only includes id and name for vitualization
+type HobbyKey struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
