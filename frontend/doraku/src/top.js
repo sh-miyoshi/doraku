146ecdd354e89c59b5009b9bf5dc3c1820e576a2
s.miyoshi <s.miyoshi1991@gmail.com>
2019-04-12 18:44:49 +0900

    add axios

--- frontend/doraku/src/top.js
+++ frontend/doraku/src/top.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router"
 import { Button } from 'react-bootstrap'
+import axios from 'axios'
 
 class Top extends Component {
-  handleToTodayPage = () => {
+  handleToTodayPage = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/api/v1/hobby/today');
+      console.log(response);
+    } catch (error) {
+      console.error(error)
+    }
     this.props.history.push('/today')
   }
 
