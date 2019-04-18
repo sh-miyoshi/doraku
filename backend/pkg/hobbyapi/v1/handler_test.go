6a2633baf8a4240b0ec6a3f620a41954d79c043b
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2019-04-18 00:02:55 +0000

    add unit test TestGetHobbyDetailsHandler

--- backend/pkg/hobbyapi/v1/handler_test.go
+++ backend/pkg/hobbyapi/v1/handler_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/sh-miyoshi/doraku/pkg/hobbydb"
 )
 
@@ -28,6 +29,7 @@ func TestGetAllHobbyHandler(t *testing.T) {
 			status, http.StatusOK)
 	}
 }
+
 func TestGetTodayHobbyHandler(t *testing.T) {
 	req, err := http.NewRequest("GET", "/api/v1/hobby/today", nil)
 	if err != nil {
@@ -48,6 +50,32 @@ func TestGetTodayHobbyHandler(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestGetHobbyDetailsHandler(t *testing.T) {
+	// Initalize DB for API call
+	hobbyFilePath := "../../../database/hobby.csv"
+	descFilePath := "../../../database/description.csv"
+	hobbydb.GetInst().Initialize(hobbyFilePath, descFilePath)
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+
+	// Test correct data
+	req, err := http.NewRequest("GET", "/api/v1/hobby/details/0", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Need to create a router that we can pass the request through so that the vars will be added to the context
+	router := mux.NewRouter()
+	router.HandleFunc("/api/v1/hobby/details/{id}", GetHobbyDetailsHandler)
+	router.ServeHTTP(rr, req)
+
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
