e59d9ac3c9327bfdcc876c8fd4d78e57038b77ad
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2019-05-16 07:32:07 +0000

    add validate method

--- backend/pkg/token/token.go
+++ backend/pkg/token/token.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"fmt"
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
@@ -12,4 +13,25 @@ func Generate() (string, error) {
 	})
 
 	return token.SignedString([]byte(testSecretKey))
+}
+
+func Validate(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(testSecretKey), nil
+	})
+
+	if err != nil {
+		return jwt.MapClaims{}, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return jwt.MapClaims{}, fmt.Errorf("Failed to validate token")
+	}
 }
