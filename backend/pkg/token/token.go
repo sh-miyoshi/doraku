6ef7fa4df1ed089712235cf89cdf04076f631ce3
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2019-05-17 00:09:48 +0000

    use StandardClaims

--- backend/pkg/token/token.go
+++ backend/pkg/token/token.go
@@ -3,21 +3,25 @@ package token
 import (
 	"fmt"
 	jwt "github.com/dgrijalva/jwt-go"
+	"time"
 )
 
+// TODO(use secure key)
 const testSecretKey = "ghoajg34qyiwgv3y4tgvobyqgqigkhiuqegwehrewhv3qha1254"
 
 func Generate() (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"name": "test",
-	})
+	claims := &jwt.StandardClaims{
+		Issuer:    "doraku",
+		ExpiresAt: time.Now().Add(time.Hour * 2).Unix(), // Expired at 2 hours
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return token.SignedString([]byte(testSecretKey))
 }
 
-func Validate(tokenString string) (jwt.MapClaims, error) {
+func Validate(tokenString string) (jwt.StandardClaims, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
@@ -26,12 +30,12 @@ func Validate(tokenString string) (jwt.MapClaims, error) {
 	})
 
 	if err != nil {
-		return jwt.MapClaims{}, err
+		return jwt.StandardClaims{}, err
 	}
 
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+	if claims, ok := token.Claims.(jwt.StandardClaims); ok && token.Valid {
 		return claims, nil
 	} else {
-		return jwt.MapClaims{}, fmt.Errorf("Failed to validate token")
+		return jwt.StandardClaims{}, fmt.Errorf("Failed to validate token")
 	}
 }
