b4064720bd7631ef7bf4de0355cc115e7ceb7ed1
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2019-05-21 08:01:17 +0000

    fix token validate bug

--- backend/pkg/token/token.go
+++ backend/pkg/token/token.go
@@ -3,16 +3,18 @@ package token
 import (
 	"fmt"
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/sh-miyoshi/doraku/pkg/logger"
 	"strings"
 	"time"
 )
 
 // TODO(use secure key)
 const testSecretKey = "ghoajg34qyiwgv3y4tgvobyqgqigkhiuqegwehrewhv3qha1254"
+const dorakuIssuer = "doraku"
 
 func Generate() (string, error) {
 	claims := &jwt.StandardClaims{
-		Issuer:    "doraku",
+		Issuer:    dorakuIssuer,
 		ExpiresAt: time.Now().Add(time.Hour * 2).Unix(), // Expired at 2 hours
 	}
 
@@ -21,8 +23,9 @@ func Generate() (string, error) {
 	return token.SignedString([]byte(testSecretKey))
 }
 
-func Validate(tokenString string) (jwt.StandardClaims, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+func validate(tokenString string) (jwt.StandardClaims, error) {
+	claims := jwt.StandardClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
@@ -34,14 +37,16 @@ func Validate(tokenString string) (jwt.StandardClaims, error) {
 		return jwt.StandardClaims{}, err
 	}
 
-	if claims, ok := token.Claims.(jwt.StandardClaims); ok && token.Valid {
+	logger.Debug("Claims: %v\n", claims)
+
+	if token.Valid {
 		return claims, nil
 	} else {
 		return jwt.StandardClaims{}, fmt.Errorf("Failed to validate token")
 	}
 }
 
-func ParseHTTPHeaderToken(tokenString string) (string, error) {
+func parseHTTPHeaderToken(tokenString string) (string, error) {
 	var splitToken []string
 	if strings.Contains(tokenString, "bearer") {
 		splitToken = strings.Split(tokenString, "bearer")
@@ -50,6 +55,36 @@ func ParseHTTPHeaderToken(tokenString string) (string, error) {
 	} else {
 		return "", fmt.Errorf("token format is missing")
 	}
-	reqToken := strings.TrimSpace(splitToken[0])
+	reqToken := strings.TrimSpace(splitToken[1])
 	return reqToken, nil
+}
+
+func Authenticate(reqToken string) error {
+	tokenStr, err := parseHTTPHeaderToken(reqToken)
+	if err != nil {
+		logger.Info("Failed to get JWT token %v", err)
+		return err
+	}
+	logger.Debug("Token String: %s", tokenStr)
+
+	claims, err := validate(tokenStr)
+	if err != nil {
+		logger.Info("Failed to auth token %v", err)
+		return err
+	}
+	logger.Debug("claims in token: %v", claims)
+
+	// Validate claims
+	if claims.Issuer != dorakuIssuer {
+		logger.Info("Issuer want %s, but got %s", dorakuIssuer, claims.Issuer)
+		return fmt.Errorf("Issuer want %s, but got %s", dorakuIssuer, claims.Issuer)
+	}
+
+	now := time.Now().Unix()
+	if now > claims.ExpiresAt {
+		logger.Info("Token is expired at %d. now: %d", claims.ExpiresAt, now)
+		return fmt.Errorf("Token is expired at %d. now: %d", claims.ExpiresAt, now)
+	}
+
+	return nil
 }
--- backend/pkg/token/token_test.go
+++ backend/pkg/token/token_test.go
@@ -4,6 +4,16 @@ import (
 	"testing"
 )
 
+// TODO Testing private method
+func TestValidate(t *testing.T) {
+	token, _ := Generate()
+	_, err := validate(token)
+	if err != nil {
+		t.Errorf("handler should pass with token %s, but got error %v", token, err)
+	}
+}
+
+// TODO Testing private method
 func TestTokenParse(t *testing.T) {
 	// Test Cases
 	tt := []struct {
@@ -18,7 +28,7 @@ func TestTokenParse(t *testing.T) {
 	}
 
 	for _, tc := range tt {
-		_, err := ParseHTTPHeaderToken(tc.token)
+		_, err := parseHTTPHeaderToken(tc.token)
 		if tc.expectPass && err != nil {
 			t.Errorf("handler should pass with token %s, but got error %v", tc.token, err)
 		}
