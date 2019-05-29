b21d61fc13aaf654f2d2376eb793d7fb59777e65
s.miyoshi <s.miyoshi1991@gmail.com>
2019-05-29 22:06:40 +0900

    add tooltip

--- frontend/doraku/src/top.js
+++ frontend/doraku/src/top.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import './top.css';
 import top_logo from './images/top_logo.png';
+import ReactTooltip from 'react-tooltip'
 
 export class Top extends Component {
   render() {
@@ -15,22 +16,23 @@ export class Top extends Component {
         <table cellPadding={5} align="center">
           <tr>
             <td>
-              <Button className="top_button" onClick={() => { this.props.history.push('/recommend') }}>
+              <Button className="top_button" data-tip="いくつかの質問からあなたにあった趣味を提案いたします" onClick={() => { this.props.history.push('/recommend') }}>
                 趣味診断
               </Button>
             </td>
             <td>
-              <Button className="top_button" onClick={() => { this.props.history.push('/today') }}>
+              <Button className="top_button" data-tip="今人気の趣味を紹介します" onClick={() => { this.props.history.push('/today') }}>
                 今日の趣味
               </Button>
             </td>
             <td>
-              <Button className="top_button" onClick={() => { this.props.history.push('/list') }}>
+              <Button className="top_button" data-tip="Dorakuで管理している趣味の一覧を表示します" onClick={() => { this.props.history.push('/list') }}>
                 趣味一覧
               </Button>
             </td>
           </tr>
         </table>
+        <ReactTooltip effect="solid" place="bottom" />
       </div>
     )
   }
