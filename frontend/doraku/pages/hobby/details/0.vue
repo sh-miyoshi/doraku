6374283f4201f6912b4f50bfadc89d9ab3da75d2
Shunsuke Miyoshi <swfft814@gmail.com>
2019-09-07 00:43:55 +0900

    update aquarium info

--- frontend/doraku/pages/hobby/details/0.vue
+++ frontend/doraku/pages/hobby/details/0.vue
@@ -7,8 +7,20 @@
     <h2>{{ hobbyName }}</h2>
     <img :src="image" alt="hobby" height="150" weight="150" />
     <div class="detail">
-      <p>説明文は準備中です・・・</p>
+      <p>
+        アクアリウム（英: aquarium）は、水生生物の飼育設備を指す。
+      </p>
+      <p>
+        日本では主に、観賞用に熱帯魚（観賞魚）や水草などを飼育・栽培すること、またはそのために構築された水槽を含む環境を指すことが多い。
+      </p>
     </div>
+    <span class="where">
+      from
+      <a
+        href="https://ja.wikipedia.org/wiki/%E3%82%A2%E3%82%AF%E3%82%A2%E3%83%AA%E3%82%A6%E3%83%A0"
+        >wikipedia</a
+      >
+    </span>
   </div>
 </template>
 
@@ -40,6 +52,12 @@ export default {
   margin-left: auto;
   border: 1px solid #000;
   overflow: auto;
+  text-align: left;
+  padding: 2px;
+}
+
+.detail > p {
+  margin: 0.2em 0px;
 }
 
 .where {
