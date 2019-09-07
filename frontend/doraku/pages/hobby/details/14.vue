ba26d8a34f11676d03512c6b86ebbbafd3b4f1e0
Shunsuke Miyoshi <swfft814@gmail.com>
2019-09-07 14:32:02 +0900

    update nail art info

--- frontend/doraku/pages/hobby/details/14.vue
+++ frontend/doraku/pages/hobby/details/14.vue
@@ -7,8 +7,18 @@
     <h2>{{ hobbyName }}</h2>
     <img :src="image" alt="hobby" height="150" weight="150" />
     <div class="detail">
-      <p>説明文は準備中です・・・</p>
+      <p>
+        ネイルアート (英語: Nail art)
+        とは、手足の爪に施す化粧や装飾のことである。ネイルアートを施してくれる店をネイルサロン、その技術者を日本では一般的にネイリストという。さまざまなネイルアート用品が市販されており、プロに限らず、自身の爪を飾る創作作業を楽しむために、ネイルアートを行う人も多くなっている
+      </p>
     </div>
+    <span class="where">
+      from
+      <a
+        href="https://ja.wikipedia.org/wiki/%E3%83%8D%E3%82%A4%E3%83%AB%E3%82%A2%E3%83%BC%E3%83%88"
+        >wikipedia</a
+      >
+    </span>
   </div>
 </template>
 
