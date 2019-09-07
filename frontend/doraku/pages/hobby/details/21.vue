00ebbe89fae280180eedd098524affe32cda3983
Shunsuke Miyoshi <swfft814@gmail.com>
2019-09-07 14:33:36 +0900

    update temple and shrine info

--- frontend/doraku/pages/hobby/details/21.vue
+++ frontend/doraku/pages/hobby/details/21.vue
@@ -7,8 +7,16 @@
     <h2>{{ hobbyName }}</h2>
     <img :src="image" alt="hobby" height="150" weight="150" />
     <div class="detail">
-      <p>説明文は準備中です・・・</p>
+      <p>
+        仏閣巡りとは、たくさんある寺社を徒歩や自転車、車で回って行く趣味です。
+        最近話題の 御朱印集め は寺社めぐりの一種です。
+        その他にも、狛犬や鳥居、仏像観察など、自分なりの注目ポイントを決めてめぐるとおもしろいですよ。
+      </p>
     </div>
+    <span class="where">
+      from
+      <a href="https://shumi.info/299">趣味サーチ</a>
+    </span>
   </div>
 </template>
 
