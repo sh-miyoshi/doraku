daa5132b9774fce762def8921ef24b41d95c65bb
Shunsuke Miyoshi <swfft814@gmail.com>
2019-09-07 13:03:33 +0900

    update futsal info

--- frontend/doraku/pages/hobby/details/6.vue
+++ frontend/doraku/pages/hobby/details/6.vue
@@ -7,8 +7,17 @@
     <h2>{{ hobbyName }}</h2>
     <img :src="image" alt="hobby" height="150" weight="150" />
     <div class="detail">
-      <p>説明文は準備中です・・・</p>
+      <p>
+        フットサル（Futsal）は、いわゆる室内サッカーである南米の「サロンフットボール」または英国の「インドアサッカー」を起源とする、基本的には室内で行われる、サッカーに似た競技である。1チーム5人で、試合時間は20分ハーフの合計40分である。
+      </p>
     </div>
+    <span class="where">
+      from
+      <a
+        href="https://ja.wikipedia.org/wiki/%E3%83%95%E3%83%83%E3%83%88%E3%82%B5%E3%83%AB"
+        >wikipedia</a
+      >
+    </span>
   </div>
 </template>
 
