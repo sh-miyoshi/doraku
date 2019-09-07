e4c821e1cf537784e9abe7c929b66ad44efcd1c4
Shunsuke Miyoshi <swfft814@gmail.com>
2019-09-07 14:33:20 +0900

    update swimming info

--- frontend/doraku/pages/hobby/details/20.vue
+++ frontend/doraku/pages/hobby/details/20.vue
@@ -7,8 +7,14 @@
     <h2>{{ hobbyName }}</h2>
     <img :src="image" alt="hobby" height="150" weight="150" />
     <div class="detail">
-      <p>説明文は準備中です・・・</p>
+      <p>
+        水泳とは水面または水中で泳ぐこと全般を指す。人が泳ぐためにはそれなりの訓練が必要となる。歴史的には、水辺で生活する上での食料採取や水難回避、移動手段、沐浴からの派生、軍事技術の一つ等として用いられてきた。近代以降はスポーツ競技としての水泳も盛んとなり、世界各地で大会が開催されている。
+      </p>
     </div>
+    <span class="where">
+      from
+      <a href="https://dic.nicovideo.jp/a/%E6%B0%B4%E6%B3%B3">ニコニコ大百科</a>
+    </span>
   </div>
 </template>
 
@@ -22,7 +28,7 @@ export default {
   },
 
   created() {
-    const HOBBY_ID = 20
+    const HOBBY_ID = 23
 
     // set hobby data
     const hobby = this.$hobby.GetHobbyByID(HOBBY_ID)
