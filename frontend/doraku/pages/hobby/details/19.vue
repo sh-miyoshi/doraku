b26e6e2fdfb4d9a679f515362ae95bd9c44457c7
Shunsuke Miyoshi <swfft814@gmail.com>
2019-09-07 14:33:11 +0900

    update skateboard info

--- frontend/doraku/pages/hobby/details/19.vue
+++ frontend/doraku/pages/hobby/details/19.vue
@@ -7,8 +7,16 @@
     <h2>{{ hobbyName }}</h2>
     <img :src="image" alt="hobby" height="150" weight="150" />
     <div class="detail">
-      <p>説明文は準備中です・・・</p>
+      <p>
+        前後に車輪がついた板に乗り、トリック（ジャンプ、空中動作、回転などの技）を行い、その技の難易度や高さ、スピードなどを評価する採点競技。東京2020大会で新たにオリンピック種目に採用される
+      </p>
     </div>
+    <span class="where">
+      from
+      <a href="https://tokyo2020.org/jp/games/sport/olympic/skateboarding/"
+        >tokyo2020</a
+      >
+    </span>
   </div>
 </template>
 
