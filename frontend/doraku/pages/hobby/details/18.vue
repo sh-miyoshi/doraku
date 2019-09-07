46f2a3d8d750e3ccd98d8a3dc3db5e30c3dff942
Shunsuke Miyoshi <swfft814@gmail.com>
2019-09-07 14:32:43 +0900

    update skate info

--- frontend/doraku/pages/hobby/details/18.vue
+++ frontend/doraku/pages/hobby/details/18.vue
@@ -7,8 +7,17 @@
     <h2>{{ hobbyName }}</h2>
     <img :src="image" alt="hobby" height="150" weight="150" />
     <div class="detail">
-      <p>説明文は準備中です・・・</p>
+      <p>
+        靴の底にブレードbladeを取り付けた，氷上を滑走するための用具（skate）。また，それを用いてスピード，ジャンプ，演技の美しさなどを競うスポーツ。スケートの歴史は，靴とブレードの改良，リンクの改善，技術の開発，が三位一体となったものである。スケートの競技には，スピードスケート，フィギュアスケート，アイスホッケー，ショートトラックの４種類がある。
+      </p>
     </div>
+    <span class="where">
+      from
+      <a
+        href="https://kotobank.jp/word/%E3%82%B9%E3%82%B1%E3%83%BC%E3%83%88-83672"
+        >kotobank</a
+      >
+    </span>
   </div>
 </template>
 
